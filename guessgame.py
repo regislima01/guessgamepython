74a27c6329adc9b4f9693944ab2f1ceb3f5726db
Regis Lima <regis.lima01@gmail.com>
2022-01-29 19:39:42 -0300

    Update guessgame.py

---
+++ guessgame.py
@@ -0,0 +1,16 @@
+print("*********************************")
+print("Welcome to the Guess Game!")
+print("*********************************")
+
+secret_number: int = 42
+
+guess_str = input("Type your number: ")
+
+print("You Type: ", guess_str)
+
+guess = int(guess_str)
+
+if(secret_number == guess):
+    print("You Won")
+else:
+    print("You lose")
